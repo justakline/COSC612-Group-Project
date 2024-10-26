00bd957f6f3dd29348d358896254a17dc3e03ed4
unknown <diegosoto9744@gmail.com>
2024-10-26 23:52:54 +0100

    Database connection configured to Heroku database now

--- traveldart/src/traveldart/DatabaseConnection.java
+++ traveldart/src/traveldart/DatabaseConnection.java
@@ -11,9 +11,9 @@ import java.sql.SQLException;
  * @author diego
  */
 public class DatabaseConnection {
-    private static final String URL = "jdbc:postgresql://localhost:5432/TravelDart";
-    private static final String USER = "postgres";
-    private static final String PASSWORD = "bondstone";
+    private static final String URL = "jdbc:postgresql://ce0lkuo944ch99.cluster-czrs8kj4isg7.us-east-1.rds.amazonaws.com:5432/d9ed3kam9kvm1d";
+    private static final String USER = "uatce3m0vi941q";
+    private static final String PASSWORD = "p4de089d14c53298bd1044127e034bb2e622f2929793b8b2251b2a0385b7159ef";
 
     public static Connection connect() {
         Connection connection = null;
