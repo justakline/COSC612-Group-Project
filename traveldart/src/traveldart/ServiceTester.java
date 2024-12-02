67f210ac56bd24e527766225ac8b7be07d6d8139
Justin Kline <50549915+justakline@users.noreply.github.com>
2024-12-01 22:08:11 -0500

    Update ServiceTester.java

--- traveldart/src/traveldart/ServiceTester.java
+++ traveldart/src/traveldart/ServiceTester.java
@@ -1,25 +1,43 @@
 package traveldart;
 
 import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import jdk.jfr.Event;
+
+import java.sql.Time;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.List;
 
 public class ServiceTester {
 
     public static void main(String[] args) {
-        Service service = new Service();
+        Service service = Service.getInstance();
         JsonArray request1;
         JsonArray request2;
         JsonArray request3;
         JsonArray request4;
         JsonArray request5;
+        JsonObject request6;
+        List<RestaurantCategories> rCategories = List.of(RestaurantCategories.TRADAMERICAN, RestaurantCategories.SEAFOOD);
+        List<EventCategories> eCategories = List.of(EventCategories.BASKETBALL,EventCategories.BASEBALL );
+        String sLocation = "7800 York Road, Towson, MD, 21252";
+        Location location = new Location(sLocation);
+        String currentTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS).toString() + "Z";
+        String endTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS).plusDays(50).toString() +"Z";
+
+        Preferences preferences = new Preferences(0, 0,rCategories, eCategories, 2,location , 10);
 
         try{
             // service.test();
             //   request1 = service.getTicketMasterEvents("Baltimore", Service.Categories.FAMILY);
             //   request2 = service.getTicketMasterEvents("Philadelphia", "Music");
             //   request3 = service.getTicketMasterEvents("Baltimore", "2024-10-03T00:00:10Z", "2024-11-22T00:00:00Z");
-            //     request4 = service.getTicketMasterEvents("Baltimore", "2024-10-03T00:00:10Z", "2024-11-22T00:00:00Z", Service.Categories.BASKETBALL);
+                request4 = service.getTicketMasterEvents(location.getCity(), currentTime.toString(), endTime.toString(), preferences.getEventPreferences());
             // service.testOpenAI();
-            request5 = service.testYelp("Towson, MD", new RestaurantCategories[]{RestaurantCategories.TRADAMERICAN, RestaurantCategories.JAPANESE},new int[]{2,3}, 2 );
+            request5 = service.getYelpRestaurants(location.toString(), preferences.getFoodPreferences(),preferences.getBudget(), preferences.getRadiusMiles() );
+            request6 = service.createRecommendation(request4, request5, preferences);
 
         }catch (Exception e){
             e.printStackTrace();
@@ -29,6 +47,7 @@ public class ServiceTester {
             request3 = new JsonArray();
             request4 = new JsonArray();
             request5 = new JsonArray();
+            request6 = new JsonObject();
         }
         //
         // System.out.println("Request 1: ");
@@ -37,9 +56,13 @@ public class ServiceTester {
         // System.out.println(Service.formatJson(request2.toString()));//
         // System.out.println("Request 3: ");
         // System.out.println(Service.formatJson(request3.toString()));
+        // System.out.println("Events: \n");
         // System.out.println("Request 4: ");
         // System.out.println(Service.formatJson(request4.toString()));
-        System.out.println("Request 5: ");
-        System.out.println(Service.formatJson(request5.toString()));
+        // System.out.println("\n\n Restaurants: \n");
+        // System.out.println("Request 5: ");
+        // System.out.println(Service.formatJson(request5.toString()));
+        System.out.println("\n\nRequest 6: ");
+        System.out.println(Service.formatJson(request6.toString()));
     }
 }
